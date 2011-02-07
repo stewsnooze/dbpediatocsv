5b01409b24780a639123403878b168f479ab024d
Stewart Robinson <stewsnooze@gmail.com>
2011-02-07 07:53:43 +0000

    Adding Ruby script to parse dbpedia XML files.

---
+++ dbpedia.rb
@@ -0,0 +1,41 @@
+#Dir.chdir("/Users/stewartrobinson/Documents/geodbpedia") 
+#file = File.new("geo_coordinates_en.nq", "r")
+file = File.new("ARGV[0]", "r")
+
+@places = {}
+last_place = "NOWHERE"
+while (line = file.gets)
+
+  # Item name
+  line = line.gsub("\^",">")
+  items = line.split(/>/)
+  items[0] = items[0].gsub("<","")
+  items[0] = items[0].gsub(/ /,"")
+  
+  items[0] = items[0].gsub("http://dbpedia.org/resource/","")
+  place_name = items[0]
+
+  if !@places.has_key?(place_name)
+    #warn place_name
+    @places[place_name] = {}
+    @places[place_name][:name] = place_name
+  end
+
+  items[1] = items[1].gsub("<","")
+  items[1] = items[1].gsub(/ /,"")
+
+  if items[1] == "http://www.w3.org/2003/01/geo/wgs84_pos#lat"
+    items[2] = items[2].gsub(/"/,"")
+    @places[place_name][:lat] = items[2]
+  end
+
+  if items[1] == "http://www.w3.org/2003/01/geo/wgs84_pos#long"
+    items[2] = items[2].gsub(/"/,"")
+    @places[place_name][:long] = items[2]
+    
+    # This next code line is a savage hack because I have to go out!
+    puts place_name + "," + @places[place_name][:lat] + "," + @places[place_name][:long]
+  end
+
+end
+file.close
