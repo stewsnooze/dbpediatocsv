bfa8a7abfc34418967d535faf7d67c581cb17f6b
root <root@centosdev.(none)>
2011-02-09 15:12:21 +0000

    amended gsubs to work inline

--- dbpedia.rb
+++ dbpedia.rb
@@ -6,10 +6,9 @@ while (line = file.gets)
   # Item name
   line = line.gsub("\^",">")
   items = line.split(/>/)
-  items[0] = items[0].gsub("<","")
-  items[0] = items[0].gsub(/ /,"")
-  
-  items[0] = items[0].gsub("http://dbpedia.org/resource/","")
+  items[0].gsub!("<","")
+  items[0].gsub!(/ /,"")
+  items[0].gsub!("http://dbpedia.org/resource/","")
   place_name = items[0]
 
   if !@places.has_key?(place_name)
@@ -18,20 +17,20 @@ while (line = file.gets)
     @places[place_name][:name] = place_name
   end
 
-  items[1] = items[1].gsub("<","")
-  items[1] = items[1].gsub(/ /,"")
+  items[1].gsub!("<","")
+  items[1].gsub!(/ /,"")
 
   if items[1] == "http://www.w3.org/2003/01/geo/wgs84_pos#lat"
-    items[2] = items[2].gsub(/"/,"")
+    items[2].gsub!(/"/,"")
     @places[place_name][:lat] = items[2]
   end
 
   if items[1] == "http://www.w3.org/2003/01/geo/wgs84_pos#long"
-    items[2] = items[2].gsub(/"/,"")
+    items[2].gsub!(/"/,"")
     @places[place_name][:long] = items[2]
     
     # This next code line is a savage hack because I have to go out!
-    puts place_name + "," + @places[place_name][:lat] + "," + @places[place_name][:long]
+    puts "#{place_name}, #@places[place_name][:lat], #@places[place_name][:long]"
   end
 
 end
